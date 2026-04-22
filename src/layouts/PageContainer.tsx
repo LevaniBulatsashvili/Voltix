4a68be79b6e346f8d4095be5aee4b4fc34981cd8
Levani <levan.bulatsashvili@gmail.com>
2026-04-22 16:34:35 +0400

    feat: add lazy loading

--- src/layouts/PageContainer.tsx
+++ src/layouts/PageContainer.tsx
@@ -1,9 +1,13 @@
+import { Skeleton } from "@/components/skeleton/Skeleton";
+import { Suspense } from "react";
 import { Outlet } from "react-router-dom";
 
 const PageContainer = () => {
   return (
     <main className="flex-1 flex flex-col justify-center items-center min-h-[88dvh]">
-      <Outlet />
+      <Suspense fallback={<Skeleton className="w-full min-h-[88dvh]" />}>
+        <Outlet />
+      </Suspense>
     </main>
   );
 };
--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,23 +1,8 @@
 import { Routes, Route, Navigate, BrowserRouter } from "react-router-dom";
 import AuthRoute from "@/routes/AuthRoute";
 import { PAGE } from "@/pages/pageConfig";
-import LoginPage from "@/features/auth/login/components";
-import RegisterPage from "@/features/auth/register/components";
-import VerifyEmailPage from "@/pages/auth/verification/VerifyEmailPage";
-import VerificationSuccessPage from "@/pages/auth/verification/VerificationSuccessPage";
-import ProductsPage from "@/pages/public/products/ProductsPage";
 import MainLayout from "@/layouts/PageLayout";
-import ProductPage from "@/pages/public/product/ProductPage";
-import ProfilePage from "@/pages/user/profile/ProfilePage";
-import CartPage from "@/pages/user/cart/CartPage";
-import SearchPage from "@/pages/public/search/SearchPage";
-import NotFoundPage from "@/pages/error/notFound/NotFoundPage";
-import SettingsPage from "@/pages/user/settings/SettingsPage";
-import CategoryPage from "@/pages/public/category/CategoryPage";
-import SearchResultsPage from "@/pages/public/searchResults/SearchResultsPage";
-import WishlistPage from "@/pages/user/wishlist/WishlistPage";
-import OrdersPage from "@/pages/user/orders/OrdersPage";
-import AdminProductPage from "@/pages/admin/product/AdminProductsPage";
+import * as Page from "@/pages/lazyPages";
 
 const AppRoutes = () => (
   <BrowserRouter>
@@ -25,45 +10,47 @@ const AppRoutes = () => (
       <Route element={<MainLayout />} path={PAGE.PUBLIC.BASE}>
         <Route index element={<Navigate to={PAGE.PUBLIC.SHOP} replace />} />
 
-        {/* Guest-only pages */}
         <Route element={<AuthRoute guestOnly />}>
-          <Route path={PAGE.AUTH.LOGIN} element={<LoginPage />} />
-          <Route path={PAGE.AUTH.REGISTER} element={<RegisterPage />} />
+          <Route path={PAGE.AUTH.LOGIN} element={<Page.LoginPage />} />
+          <Route path={PAGE.AUTH.REGISTER} element={<Page.RegisterPage />} />
         </Route>
 
         <Route element={<AuthRoute verifyPagesOnly />}>
-          <Route path={PAGE.AUTH.VERIFY_EMAIL} element={<VerifyEmailPage />} />
+          <Route
+            path={PAGE.AUTH.VERIFY_EMAIL}
+            element={<Page.VerifyEmailPage />}
+          />
           <Route
             path={PAGE.AUTH.VERIFY_SUCCESS}
-            element={<VerificationSuccessPage />}
+            element={<Page.VerificationSuccessPage />}
           />
         </Route>
 
-        {/* Public pages — accessible regardless of auth state */}
-        <Route path={PAGE.PUBLIC.SHOP} element={<ProductsPage />} />
-        <Route path={PAGE.PUBLIC.PRODUCT} element={<ProductPage />} />
-        <Route path={PAGE.PUBLIC.SEARCH} element={<SearchPage />} />
-        <Route path={PAGE.PUBLIC.CATEGORY} element={<CategoryPage />} />
+        <Route path={PAGE.PUBLIC.SHOP} element={<Page.ProductsPage />} />
+        <Route path={PAGE.PUBLIC.PRODUCT} element={<Page.ProductPage />} />
+        <Route path={PAGE.PUBLIC.SEARCH} element={<Page.SearchPage />} />
+        <Route path={PAGE.PUBLIC.CATEGORY} element={<Page.CategoryPage />} />
         <Route
           path={PAGE.PUBLIC.SEARCH_RESULTS}
-          element={<SearchResultsPage />}
+          element={<Page.SearchResultsPage />}
         />
 
-        {/* Protected pages — logged-in users only */}
         <Route element={<AuthRoute requireAuth />}>
-          <Route path={PAGE.USER.PROFILE} element={<ProfilePage />} />
-          <Route path={PAGE.USER.ORDERS} element={<OrdersPage />} />
-          <Route path={PAGE.USER.WISHLIST} element={<WishlistPage />} />
-          <Route path={PAGE.USER.SETTINGS} element={<SettingsPage />} />
-          <Route path={PAGE.USER.CART} element={<CartPage />} />
+          <Route path={PAGE.USER.PROFILE} element={<Page.ProfilePage />} />
+          <Route path={PAGE.USER.ORDERS} element={<Page.OrdersPage />} />
+          <Route path={PAGE.USER.WISHLIST} element={<Page.WishlistPage />} />
+          <Route path={PAGE.USER.SETTINGS} element={<Page.SettingsPage />} />
+          <Route path={PAGE.USER.CART} element={<Page.CartPage />} />
         </Route>
 
-        {/* Admin-only pages */}
         <Route element={<AuthRoute requireAuth allowedRoles={"admin"} />}>
-          <Route path={PAGE.ADMIN.PRODUCTS} element={<AdminProductPage />} />
+          <Route
+            path={PAGE.ADMIN.PRODUCTS}
+            element={<Page.AdminProductPage />}
+          />
         </Route>
 
-        <Route path="*" element={<NotFoundPage />} />
+        <Route path="*" element={<Page.NotFoundPage />} />
       </Route>
     </Routes>
   </BrowserRouter>
